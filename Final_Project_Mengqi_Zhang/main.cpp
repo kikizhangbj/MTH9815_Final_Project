5be61a3456b9b0289fd9b694ff6dc2b84b6c5d0a
fruitmaomao <kikizhangbj@gmail.com>
2016-12-09 11:14:35 -0500

    Initial Commit

---
+++ Final_Project_Mengqi_Zhang/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final_Project_Mengqi_Zhang
+//
+//  Created by Mengqi Zhang on 12/9/16.
+//  Copyright © 2016 Mengqi Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
